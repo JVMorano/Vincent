5f03cffa2a9a8d199c8b2c0b2bf054d932015ebf
unknown <Admin@CSLAB207.CSSERVER.local>
2024-02-01 19:36:00 +0800

    Aritmethi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aritmethic
+{
+
+    public class Math
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            int num1, num2;
+
+            //Add
+            Console.WriteLine("Additional");
+            Console.WriteLine("Enter num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total = " + math.Add(num1, num2));
+
+
+
+            //Sub
+            Console.WriteLine("Subtract");
+            Console.WriteLine("Enter num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total = " + math.Sub(num1, num2));
+
+
+
+            //Mul
+            Console.WriteLine("Multiply");
+            Console.WriteLine("Enter num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total = " + math.Mul(num1, num2));
+
+
+
+            //Div
+            Console.WriteLine("Division");
+            Console.WriteLine("Enter num1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total = " + math.Div(num1, num2));
+
+
+        }
+
+
+    }
+
+}
+
+
+
--- ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aade241bb7538df48d4860bdc19b610b8f3006c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
